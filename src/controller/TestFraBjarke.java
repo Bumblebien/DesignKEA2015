d217b2166bdb2878bef2bbc83895f30cef6a7738
Bumblebien <mikkel.honore@gmail.com>
2015-05-07 09:29:27 +0200

    Update 5.5
    udkommenteret kode

--- src/controller/TestFraBjarke.java
+++ src/controller/TestFraBjarke.java
@@ -1,8 +1,8 @@
-package controller;
-
-public class TestFraBjarke {
-
-	public static void main(String[] args) {
-		System.out.println("BJARKE SUCKS");
-	}
-}
+//package controller;
+//
+//public class TestFraBjarke {
+//
+//	public static void main(String[] args) {
+//		System.out.println("BJARKE SUCKS");
+//	}
+//}
